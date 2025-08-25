1ac10fb713c99b11c81d02b57ea14b56b698bfa1
DevelopmentPc <ashish.yadav@vivo.com>
2025-08-25 10:51:35 +0530

    Created all prop in user

--- Models/User.cs
+++ Models/User.cs
@@ -2,16 +2,14 @@
 {
     public class User
     {
-        public int Id { get; set; }
+
+        public int TblId { get; set; }
+        public string UserId { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; } = string.Empty;
-        //public int TblId { get; set; }
-        //public string UserId { get; set; }
-        //public string UserName { get; set; }
-        //public string PasswordHash { get; set; } = string.Empty;
-        //public string? UserType { get; set; }
-        //public int ? MPIN { get; set; }
-        //public string SecurityQuestion { get; set; } = string.Empty;
-        //public string SecurityAnswer { get; set; } = string.Empty;
+        public string? UserType { get; set; }
+        public int? MPIN { get; set; }
+        public string SecurityQuestion { get; set; } = string.Empty;
+        public string SecurityAnswer { get; set; } = string.Empty;
     }
 }
