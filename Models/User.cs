bdc527573b61fa3cc4f7ad62cf7d85030d5e6790
DevelopmentPc <ashish.yadav@vivo.com>
2025-08-25 09:30:32 +0530

    Revert "Changes in User model"
    This reverts commit c50deb8d839b63f5cf1cb6174c7ba1f7e5b03d5f.

--- Models/User.cs
+++ Models/User.cs
@@ -2,13 +2,8 @@
 {
     public class User
     {
-        public int TblId { get; set; }
-        public string UserId { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; } = string.Empty;
-        public string? UserType { get; set; }
-        public int? MPIN { get; set; }
-        public string SecurityQuestion { get; set; } = string.Empty;
-        public string SecurityAnswer { get; set; } = string.Empty;
     }
 }
