621910dddec70d045494153a1d45c6e3b0d1d0a7
DevelopmentPc <ashish.yadav@vivo.com>
2025-08-25 10:51:06 +0530

    change in user

--- Models/User.cs
+++ Models/User.cs
@@ -2,13 +2,16 @@
 {
     public class User
     {
-        public int TblId { get; set; }
-        public string UserId { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; } = string.Empty;
-        public string? UserType { get; set; }
-        public int ? MPIN { get; set; }
-        public string SecurityQuestion { get; set; } = string.Empty;
-        public string SecurityAnswer { get; set; } = string.Empty;
+        //public int TblId { get; set; }
+        //public string UserId { get; set; }
+        //public string UserName { get; set; }
+        //public string PasswordHash { get; set; } = string.Empty;
+        //public string? UserType { get; set; }
+        //public int ? MPIN { get; set; }
+        //public string SecurityQuestion { get; set; } = string.Empty;
+        //public string SecurityAnswer { get; set; } = string.Empty;
     }
 }
