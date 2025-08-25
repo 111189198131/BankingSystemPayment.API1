220097137f08ea764699c67ad19a3eee49c58adc
DevelopmentPc <ashish.yadav@vivo.com>
2025-08-25 09:15:17 +0530

    Change in User

--- Models/User.cs
+++ Models/User.cs
@@ -2,8 +2,13 @@
 {
     public class User
     {
-        public int Id { get; set; }
+        public int TblId { get; set; }
+        public string UserId { get; set; }
         public string UserName { get; set; }
         public string PasswordHash { get; set; } = string.Empty;
+        public string? UserType { get; set; }
+        public int ? MPIN { get; set; }
+        public string SecurityQuestion { get; set; } = string.Empty;
+        public string SecurityAnswer { get; set; } = string.Empty;
     }
 }
