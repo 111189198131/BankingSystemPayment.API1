c9d751e7488f7df025d73b194b43beae0379cb6c
ashish <ashishyadav6217@gmail.com>
2025-08-25 21:50:58 +0530

    Model Changes

---
+++ Models/Tbl_Login.cs
@@ -0,0 +1,12 @@
+﻿namespace BankingSystemPayment.API.Models
+{
+    public class Tbl_Login
+    {
+        public int TblId { get; set; }
+        public string UserId { get; set; }
+        public string UseName { get; set; }
+        public string? UserType { get; set; }
+        public string? SecurityQuestion { get; set; }
+        public string? SecurityAnswer { get; set; }
+    }
+}
---
+++ Models/Tbl_UserDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace BankingSystemPayment.API.Models
+{
+    public class Tbl_UserDetails
+    {
+        public int TblId { get; set; }
+        public string UserId { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Address { get; set; }
+        public string BankName { get; set; }
+        public string Nomnee { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string PAN { get; set; }
+    }
+}
